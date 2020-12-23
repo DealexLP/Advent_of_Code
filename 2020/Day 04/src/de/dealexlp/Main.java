faf78f972cf647cf9259f07cd8ff02a5c04530f1
David Dunkel <dasadu544@gmail.com>
2020-12-23 14:08:24 +0100

    update
    Signed-off-by: David Dunkel <dasadu544@gmail.com>

--- 2020/Day 04/src/de/dealexlp/Main.java
+++ 2020/Day 04/src/de/dealexlp/Main.java
@@ -3,8 +3,8 @@ package de.dealexlp;
 public class Main {
 
     public static void main(String[] args) {
-        String[] lines = Utils.readInput();
+        final String[] lines = Utils.readInput();
         Part01Solver.solve(lines);
-        //de.dealexlp.Part02Solver.solve(lines);
+        Part02Solver.solve(lines);
     }
 }
